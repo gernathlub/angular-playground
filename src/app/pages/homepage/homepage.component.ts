c4da961ff48855d0bd7952993e76abd9712023f1
Lubomir Gernath <gernathlub@gmail.com>
2025-02-25 17:33:40 +0100

    feat: button with input & output

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
