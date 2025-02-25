3195c909470254f421e50f9b97f6cc1fbbac8c60
Lubomir Gernath <gernathlub@gmail.com>
2025-02-25 17:15:54 +0100

    feat: homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
+import { HomepageComponent } from "./pages/homepage/homepage.component";
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, FooterComponent],
+    imports: [HeaderComponent, FooterComponent, HomepageComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
